0216358432c1ac4e408843861f19459e213d2c2a
Vladislav <sidorenkov.slava@gmail.com>
2020-10-23 21:16:36 +0300

    all user tests done

--- users/serializers.py
+++ users/serializers.py
@@ -3,13 +3,9 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    role = serializers.SerializerMethodField(method_name='get_role')
-
     class Meta:
         fields = (
             'first_name', 'last_name', 'username', 'bio', 'email', 'role'
         )
         model = User
 
-    def get_role(self, obj):
-        return obj.get_role()
