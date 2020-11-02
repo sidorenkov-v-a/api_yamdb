4c4ded427862c9b804e375d121502a02afb9ddca
uralchelyabinsk <vasilij.korobkin@yandex.ru>
2020-11-02 17:26:25 +0500

    all comments done

--- reviews/views.py
+++ reviews/views.py
@@ -23,11 +23,6 @@ class ReviewViewSet(ModelViewSet):
         queryset = title.reviews.all()
         return queryset
 
-    def get_serializer_context(self):
-        title_id = self.kwargs.get('title_id')
-        title = get_object_or_404(Title, pk=title_id)
-        return {'title': title, 'request': self.request}
-
     def perform_create(self, serializer):
         title_id = self.kwargs.get('title_id')
         title = get_object_or_404(Title, pk=title_id)
